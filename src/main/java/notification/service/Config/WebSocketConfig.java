1257ed36f81cf2248b689c7d1f5e9535680d311b
Zavis <thezavis@gmail.com>
2025-02-05 01:55:36 +1100

    Config WebSocket change from allowOrigin to allowOriginPatterns

--- src/main/java/notification/service/Config/WebSocketConfig.java
+++ src/main/java/notification/service/Config/WebSocketConfig.java
@@ -7,18 +7,20 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-@EnableWebSocketMessageBroker
+@EnableWebSocketMessageBroker // publish-subcribe model
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
+		//client subscribe to topic /topic/notification to receive message send from server
 		registry.enableSimpleBroker("/topic");
 		
+		//client send message to server
 		registry.setApplicationDestinationPrefixes("/app");
 	}
 	
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+		registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
 	}
 }
